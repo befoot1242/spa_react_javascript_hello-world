e6f64b8e0db9629b24b0bb93282c191e9a6ef404
befoot1242 <uchikoshi12@gmail.com>
2025-03-29 10:41:05 +0900

    Learn_Auth0Springへの接続を設定

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -4,7 +4,7 @@ const apiServerUrl = process.env.REACT_APP_API_SERVER_URL;
 
 export const getPublicResource = async () => {
   const config = {
-    url: `${apiServerUrl}/api/messages/public`,
+    url: `${apiServerUrl}/api/public`,
     method: "GET",
     headers: {
       "content-type": "application/json",
@@ -21,7 +21,7 @@ export const getPublicResource = async () => {
 
 export const getProtectedResource = async () => {
   const config = {
-    url: `${apiServerUrl}/api/messages/protected`,
+    url: `${apiServerUrl}/api/private`,
     method: "GET",
     headers: {
       "content-type": "application/json",
